ddbefa94e300b065fe64ff14f4d333c2453762c2
LeGenius2022 <zwritingexpert2021@gmail.com>
2024-08-01 17:46:41 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const ham = document.querySelector(".header-ham");
-    const nav = document.querySelector("nav ul");
-
-    ham.addEventListener("click", function() {
-        nav.classList.toggle("open");
-    });
-});
